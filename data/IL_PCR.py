7d4ecaad3b78a6845c633db7af29c9109c377468
Damith Premasiri <damithpremasiri@gmail.com>
2024-09-23 16:54:16 +0100

    testing

--- data/IL_PCR.py
+++ data/IL_PCR.py
@@ -30,7 +30,7 @@ class IL_PCR(DataClass):
         candidates = self.candidates['text']
         candidates = [' \n'.join(candidate) for candidate in candidates]
         # candidates = [candidate[1] for candidate in candidates] #todo change after testing
-        candidates = candidates[:100] #todo remove after testing
+        # candidates = candidates[:100] #todo remove after testing
 
         candidates = ['hi how are you', 'I am fine thank you']
         tokeniser = AutoTokenizer.from_pretrained(model_name)
