0bfc8bf1bae0b7098d34d31e57d0c3ac6c1956be
Damith Premasiri <damithpremasiri@gmail.com>
2024-09-24 15:12:04 +0100

    testing

--- test_case.py
+++ test_case.py
@@ -4,7 +4,7 @@ import torch
 import torch.nn.functional as F
 from datasets import load_dataset
 from torch.nn import DataParallel
-from transformers import AutoTokenizer, AutoModel
+from transformers import AutoTokenizer, AutoModel, pipeline
 import torch.distributed as dist
 from torch.nn.parallel import DistributedDataParallel as DDP
 
@@ -43,10 +43,12 @@ def run(args):
 
     # load model with tokenizer
     # Initialize the process group for distributed training
-    dist.init_process_group(backend="nccl")
-    model = AutoModel.from_pretrained('nvidia/NV-Embed-v2', device_map='auto', trust_remote_code=True)
-    model = model.cuda()
-    model = DDP(model, device_ids=[0,1,2])
+
+    # model = AutoModel.from_pretrained('nvidia/NV-Embed-v2', device_map='auto', trust_remote_code=True)
+    pipe = pipeline("feature-extraction", framework="pt", model='nvidia/NV-Embed-v2', device_map="auto", trust_remote_code=True,
+                    tokenizer=tokeniser)
+    features = pipe(passages, return_tensors="pt", batch_size=1)
+    print(features)
 
     # if torch.cuda.is_available():
     #     print('cuda is available shifting data to cuda')
@@ -55,24 +57,24 @@ def run(args):
     #     for module_key, module in model._modules.items():
     #         model._modules[module_key] = DataParallel(module)
 
-    tokenised_data = tokenised_data.to('cuda')
+    # tokenised_data = tokenised_data.to('cuda')
 
-    # get the embeddings
-    max_length = 32768
-    query_embeddings = model.encode(queries, instruction=query_prefix, max_length=max_length)
-    passage_embeddings = model.encode(tokenised_data, instruction=passage_prefix, max_length=max_length)
+    # # get the embeddings
+    # max_length = 32768
+    # query_embeddings = model.encode(queries, instruction=query_prefix, max_length=max_length)
+    # passage_embeddings = model.encode(tokenised_data, instruction=passage_prefix, max_length=max_length)
 
-    # normalize embeddings
-    query_embeddings = F.normalize(query_embeddings, p=2, dim=1)
-    passage_embeddings = F.normalize(passage_embeddings, p=2, dim=1)
+    # # normalize embeddings
+    # query_embeddings = F.normalize(query_embeddings, p=2, dim=1)
+    # passage_embeddings = F.normalize(passage_embeddings, p=2, dim=1)
 
     # get the embeddings with DataLoader (spliting the datasets into multiple mini-batches)
     # batch_size=2
     # query_embeddings = model._do_encode(queries, batch_size=batch_size, instruction=query_prefix, max_length=max_length, num_workers=32, return_numpy=True)
     # passage_embeddings = model._do_encode(passages, batch_size=batch_size, instruction=passage_prefix, max_length=max_length, num_workers=32, return_numpy=True)
 
-    scores = (query_embeddings @ passage_embeddings.T) * 100
-    print(scores.tolist())
+    # scores = (query_embeddings @ passage_embeddings.T) * 100
+    # print(scores.tolist())
     # [[87.42693328857422, 0.46283677220344543], [0.965264618396759, 86.03721618652344]]
 
 
