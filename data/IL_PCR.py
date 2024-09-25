487019c86eaa43a0a8796c27a443acf90b6a2f77
Damith Premasiri <damithpremasiri@gmail.com>
2024-09-25 15:56:02 +0100

    sentence transformers

--- data/IL_PCR.py
+++ data/IL_PCR.py
@@ -25,32 +25,30 @@ class IL_PCR(DataClass):
     def load_queries(self, dataset):
         self.candidates = load_dataset(dataset, "pcr", split='test_queries')
 
+    def add_eos(self, input_examples):
+        input_examples = [input_example + self.model.tokenizer.eos_token for input_example in input_examples]
+        return input_examples
+
     def vectorise_candidates(self, model_name=None):
         self.load_candidates(self.dataset)
         candidates = self.candidates['text']
         candidates = [' \n'.join(candidate) for candidate in candidates]
-        # candidates = [candidate[1] for candidate in candidates] #todo change after testing
-        # candidates = candidates[:100] #todo remove after testing
-
-        candidates = ['hi how are you', 'I am fine thank you']
-        tokeniser = AutoTokenizer.from_pretrained(model_name)
-
-        if torch.cuda.is_available():
-            num_devices = torch.cuda.device_count()
-            print(f'visible devices {num_devices}')
-        pipe = pipeline("feature-extraction", framework="pt", model=model_name,device_map="auto",trust_remote_code=True,tokenizer=tokeniser)
-
-        print(candidates)
-        features = pipe(candidates, return_tensors="pt", batch_size=1)
-        print(features)
 
         # candidate_embeddings = embedding_model.encode(candidates, instruction="", max_length=self.max_length)
 
+        self.model = SentenceTransformer(model_name, trust_remote_code=True)
+        self.model.max_seq_length = 32768
+        self.model.tokenizer.padding_side = "right"
 
+        # get the embeddings
+        batch_size = 2
+        candidate_embeddings = self.model.encode(self.add_eos(candidates), show_progress_bar=True,
+                                                 batch_size=batch_size,
+                                                 normalize_embeddings=True)
 
-        # embeddings_df = pd.DataFrame(candidate_embeddings)
-        # model_alias = model_name.split('/')[-1] if model_name.__contains__('/') else model_name
-        # dataset_alias = self.dataset.split('/')[-1] if self.dataset.__contains__('/') else self.dataset
-        # save_path = f'embeddings/{model_alias}/{dataset_alias}.csv'
-        # os.makedirs(save_path)
-        # embeddings_df.to_csv(save_path, index=False)
+        embeddings_df = pd.DataFrame(candidate_embeddings)
+        model_alias = model_name.split('/')[-1] if model_name.__contains__('/') else model_name
+        dataset_alias = self.dataset.split('/')[-1] if self.dataset.__contains__('/') else self.dataset
+        save_path = f'embeddings/{model_alias}/{dataset_alias}.csv'
+        os.makedirs(save_path)
+        embeddings_df.to_csv(save_path, index=False)
--- test.py
+++
@@ -1,6 +0,0 @@
-from data.DataClass import DataClass
-from data.IL_PCR import IL_PCR
-
-if __name__ == '__main__':
-    dataset = IL_PCR(32000)
-    dataset.vectorise_candidates()
