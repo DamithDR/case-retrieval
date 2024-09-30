cbaae6a0e528a4ae08a25b00ef0d343f87c478c2
Damith Premasiri <damithpremasiri@gmail.com>
2024-09-30 19:27:17 +0100

    commit runner

--- runner.sh
+++ runner.sh
@@ -1,1 +1,6 @@
+#!/bin/bash
 python -m preprocess.vectoriser --model_name dunzhang/stella_en_1.5B_v5 --dataset IL_PCR
+python -m preprocess.vectoriser --model_name dunzhang/stella_en_1.5B_v5 --dataset coliee
+python -m preprocess.vectoriser --model_name dunzhang/stella_en_1.5B_v5 --dataset ilpcr
+python -m preprocess.vectoriser --model_name dunzhang/stella_en_1.5B_v5 --dataset muser
+python -m preprocess.vectoriser --model_name dunzhang/stella_en_1.5B_v5 --dataset ecthr
