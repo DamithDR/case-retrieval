ef11d02bf7949e1bc324e99ae4c8a4735775fbe8
Damith Premasiri <damithpremasiri@gmail.com>
2024-10-07 13:16:08 +0100

    batch size 2

--- models/nvembedv2.py
+++ models/nvembedv2.py
@@ -9,7 +9,7 @@ class Nvembedv2(AbsEmbed):
 
         self.model.max_seq_length = 3072
         self.model.tokenizer.padding_side = "right"
-        self.batch_size = 4
+        self.batch_size = 2
 
     def vectorise(self, data):
         embeddings = self.model.encode(data, show_progress_bar=True,
