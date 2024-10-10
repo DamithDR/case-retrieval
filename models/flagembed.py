ab0497e644a27504346268b7b638c252d84a5f7c
Damith Premasiri <damithpremasiri@gmail.com>
2024-10-10 18:12:23 +0100

    change batch size

--- models/flagembed.py
+++ models/flagembed.py
@@ -11,7 +11,7 @@ class flag(AbsEmbed):
 
         self.max_seq_length = 3072
         self.model.tokenizer.padding_side = "right"
-        self.batch_size = 16
+        self.batch_size = 8
 
     def vectorise(self, data):
         embeddings = self.model.encode(data, show_progress_bar=True,
