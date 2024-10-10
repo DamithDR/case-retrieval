2e9d5cfd8484bf75d92dd1b64d3401971b764a1d
Damith Premasiri <damithpremasiri@gmail.com>
2024-10-11 00:04:02 +0100

    model to cuda

--- models/nvembedv2.py
+++ models/nvembedv2.py
@@ -7,9 +7,9 @@ from models.absembed import AbsEmbed
 class Nvembedv2(AbsEmbed):
     def __init__(self):
         super().__init__('nvidia/NV-Embed-v2')
-        self.model = AutoModel.from_pretrained('nvidia/NV-Embed-v2', trust_remote_code=True)
+        self.model = AutoModel.from_pretrained('nvidia/NV-Embed-v2', trust_remote_code=True).to('cuda')
 
-        self.model.max_seq_length = 3072
+        self.max_seq_length = 3072
         self.model.tokenizer.padding_side = "right"
         self.batch_size = 8
 
@@ -20,7 +20,7 @@ class Nvembedv2(AbsEmbed):
                 batch_data = data[i:]
             else:
                 batch_data = data[i:i + self.batch_size]
-            batch_embeddings = self.model.encode(batch_data, instruction="", max_length=self.model.max_seq_length)
+            batch_embeddings = self.model.encode(batch_data, instruction="", max_length=self.max_seq_length)
             batch_embeddings = F.normalize(batch_embeddings, p=2, dim=1)
             all_embeddings.append(batch_embeddings)
         return all_embeddings
